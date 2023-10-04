7297b2018a3bfcbfd5d625112021c2b16875bb84
amikom <antamaulana914@gmail.com>
2023-10-04 14:53:53 +0700

    addfragment

---
+++ app/src/main/java/com/example/project_2638/BookKidActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project_2638
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class BookKidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_kid)
+    }
+}
---
+++ app/src/main/java/com/example/project_2638/activity_login.kt
@@ -0,0 +1,10 @@
+package com.example.project_2638
+
+import abdroid
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
